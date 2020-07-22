95d8946391f01bef02e4ca126338e6a767e1bbc8
Vasanth Kumar <vasanthkumar.j@outlook.com>
2020-07-22 10:37:46 +0530

    added full name

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,6 +1,6 @@
 public class TestMain{
     public static void main(String[] args) {
-             String s= "vasanth";
+             String s= "vasanth kumar";
         System.out.println(s);
     }
 }
